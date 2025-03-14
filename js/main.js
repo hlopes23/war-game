8aeb9eaa91293d219e7953180908a509a20edbe4
mindera teste <henrique.silva.lopes@outlook.pt>
2025-03-14 16:58:43 +0000

    styles edited and game logic solved

--- js/main.js
+++ js/main.js
@@ -1,123 +1,124 @@
-
 // CREATE DECK ()
-    const generateDeck = () => {
-      const suits = ["hearts", "diamonds", "spades", "clubs"];
+const generateDeck = () => {
+  const suits = ["hearts", "diamonds", "spades", "clubs"];
 
-      const values = [
-        "1",
-        "2",
-        "3",
-        "4",
-        "5",
-        "6",
-        "7",
-        "8",
-        "9",
-        "10",
-        "11",
-        "12",
-        "13",
-      ];
+  const values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+  ];
 
-      let deck = [];
-
-      for (let suit of suits) {
-        for (let value of values) {
-          const card = { suit, value };
-          deck.push(card);
-        }
-      }
-      return deck;
-    };
-
-    deck = generateDeck();
-
-
-    // SHUFFLE DECK ()
-    const shuffleDeck = (deck) => {
-      for (let i = deck.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [deck[i], deck[j]] = [deck[j], deck[i]];
-      }
-      return deck;
-    };
-
-    // DEAL CARDS ()
-    const dealCards = () => {
-      shuffleDeck(deck);
-      const half = Math.floor(deck.length / 2);
-      const deck1 = deck.slice(0, half);
-      const deck2 = deck.slice(half);
-
-      return { deck1, deck2 };
-    };
-
-    let { deck1, deck2 } = dealCards();
-
-
-    // PLAY ()
-    const play = (deck1, deck2) => {
-      let card1 = deck1.shift();
-      let card2 = deck2.shift();
-
-      if (card1.value > card2.value) {
-        console.log("Player 1 wins this round.");
-        deck1.push(card1, card2);
-      } else {
-        console.log("Player 2 wins this round.");
-        deck2.push(card1, card2);
-      }
-
-      if (card1.value === card2.value && deck1.length > 3 && deck2.length > 3) {
-        console.log("It's a tie.");
-        let tempStack = [];
-
-        for (i = 0; i <= 2; i++) {
-          card1 = deck1.shift();
-          card2 = deck2.shift();
-          tempStack.push(card1, card2);
-        }
-        console.log(tempStack);
-        card1 = deck1.shift();
-        card2 = deck2.shift();
-
-        card1.value > card2.value
-          ? deck1.push(...tempStack, card1, card2) |
-            console.log("Player 1 gets " + (tempStack.length + 4) + " Cards")
-          : deck2.push(...tempStack, card1, card2) |
-            console.log("Player 2 gets " + (tempStack.length + 4) + " Cards");
-      }
-
-      console.log(deck1, deck2);
-    };
-
-
-    // DEAL CARDS BUTTON
-    document.querySelector(".deal").addEventListener("click", () => {
-      const { deck1, deck2 } = dealCards();
-      document.querySelector(".play").disabled = false;
-      console.log(deck1, deck2);
+  for (let suit of suits) {
+    //for (let value of values) {
+    values.forEach((text, value) => {
+      value = value + 1;
+      const card = { suit, text, value };
+      deck.push(card);
     });
+  }
+};
+
+// SHUFFLE DECK ()
+const shuffleDeck = (deck) => {
+  for (let i = deck.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [deck[i], deck[j]] = [deck[j], deck[i]];
+  }
+};
+
+// DEAL CARDS ()
+const dealCards = () => {
+  if(deck.length===0)
+    generateDeck();
+  shuffleDeck(deck);
+  const half = Math.floor(deck.length / 2);
+  deck1 = deck.slice(0, half);
+  deck2 = deck.slice(half);
+};
+
+// PLAY ()
+const play = () => {
+  
+  shuffleDeck(deck1);
+  shuffleDeck(deck2);
+  
+  let card1 = deck1.shift();
+  let card2 = deck2.shift();
+
+  if (card1.value > card2.value) {
+    console.log("Player 1 wins this round.");
+    deck1.push(card1, card2);
+  } else {
+    console.log("Player 2 wins this round.");
+    deck2.push(card1, card2);
+  }
+
+  if (card1.value === card2.value && deck1.length > 3 && deck2.length > 3) {
+    console.log("It's a tie.");
+    let tempStack = [];
+
+    for (i = 0; i <= 2; i++) {
+      card1 = deck1.shift();
+      card2 = deck2.shift();
+      tempStack.push(card1, card2);
+    }
+    console.table(tempStack);
+    card1 = deck1.shift();
+    card2 = deck2.shift();
+
+    card1.value > card2.value
+      ? deck1.push(...tempStack, card1, card2) |
+        console.log("Player 1 gets " + (tempStack.length + 4) + " Cards")
+      : deck2.push(...tempStack, card1, card2) |
+        console.log("Player 2 gets " + (tempStack.length + 4) + " Cards");
+  }
+
+  console.table(deck1, deck2);
+};
 
 
-    // PLAY BUTTON
-    document.querySelector(".play").addEventListener("click", () => {
-      while (deck1.length !== 0 && deck2.length !== 0) {
-        play(deck1, deck2);
-        console.log(
-          "PLayer 1: " +
-            deck1.length +
-            " cards." +
-            "\n" +
-            "Player 2: " +
-            deck2.length +
-            " cards."
-        );
-      }
 
-      if (deck1.length === 0) {
-        alert("Player 2 wins.");
-      } else if (deck2.length === 0) {
-        alert("Player 1 wins.");
-      }
-    });
+let deck1 = [];
+let deck2 = [];
+let deck = [];
+
+
+// DEAL CARDS BUTTON
+document.querySelector(".deal").addEventListener("click", () => {
+  dealCards();
+  document.querySelector(".play").disabled = false;
+  console.table(deck1); 
+   console.table( deck2);
+});
+
+// PLAY BUTTON
+document.querySelector(".play").addEventListener("click", () => {
+  while (deck1.length !== 0 && deck2.length !== 0) {
+    play();
+    console.log(
+      "PLayer 1: " +
+        deck1.length +
+        " cards." +
+        "\n" +
+        "Player 2: " +
+        deck2.length +
+        " cards."
+    );
+  }
+
+  if (deck1.length === 0) {
+    alert("Player 2 wins.");
+  } else if (deck2.length === 0) {
+    alert("Player 1 wins.");
+  }
+});
